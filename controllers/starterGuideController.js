757a4d26a38bfb1bf1417eb769b944f7b570e73b
Kevin2Holt <kevin2holt@gmail.com>
2024-05-01 08:59:59 -0400

    Added Getting Started Content
    Drafted and added content to the getting started pages

--- controllers/starterGuideController.js
+++ controllers/starterGuideController.js
@@ -1,3 +1,15 @@
 module.exports.get_gettingStarted = (req,res) => {
 	res.render("gettingStarted");
 }
+module.exports.get_cardOverview = (req,res) => {
+	res.render("cardOverview");
+}
+module.exports.get_cardTypes = (req,res) => {
+	res.render("cardTypes");
+}
+module.exports.get_boardZones = (req,res) => {
+	res.render("boardZones");
+}
+module.exports.get_turnOverview = (req,res) => {
+	res.render("turnOverview");
+}
--- routers/starterGuideRouter.js
+++ routers/starterGuideRouter.js
@@ -3,6 +3,10 @@ const controller = require("../controllers/starterGuideController");
 
 const router = express.Router();
 
+router.get("/card-overview", controller.get_cardOverview);
+router.get("/card-types", controller.get_cardTypes);
+router.get("/board-zones", controller.get_boardZones);
+router.get("/turn-overview", controller.get_turnOverview);
 router.get("/", controller.get_gettingStarted);
 
 module.exports = router;
