a73fed152d1ab1ca408313fd8bf4ec84d441b57f
Kevin2Holt <kevin2holt@gmail.com>
2024-05-04 16:38:35 -0400

    Quiz Partial 2

--- public/script.js
+++ public/script.js
@@ -1,10 +1,13 @@
 var currentQuestion = 0;// 0 is the first question (like arrays)
 
-function switchQuestion(questionNum) {// questionNum starts at 0
-	$("#question_"+currentQuestion)
+function switchQuestion(questionNum = currentQuestion+1) {// questionNum starts at 0
+	document.getElementById("question_"+currentQuestion).style.display = "none";
+	document.getElementById("question_"+questionNum).style.display = "inline-block";
+	currentQuestion = questionNum;
 }
 
 
+
+
 $(function() {// On document ready
-	startQuiz();
 });
